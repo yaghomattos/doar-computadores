907a131c3c865049d3dd6ab236766a007b509860
Yagho Mattos <mattosyagho@gmail.com>
2022-07-22 17:36:53 -0300

    chamada get com exibição no console

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
+export async function getStaticProps() {
+  const data = await fetch('https://doar-computador-api.herokuapp.com/');
+
+  const alive = await data.json();
+
+  return {
+    props: {
+      result: alive,
+    },
+  };
+}
+
+export default function Home({ result }) {
+  console.log(result);
+
   return <h1>Doação de computadores usados</h1>;
 }
