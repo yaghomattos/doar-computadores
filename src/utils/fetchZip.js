9b973942512c68a8fc0d2ea846e6a80fe88f8c2c
Yagho Mattos <mattosyagho@gmail.com>
2022-08-02 23:49:55 -0300

    feat: try, catch in fecthZip

--- src/utils/fetchZip.js
+++ src/utils/fetchZip.js
@@ -7,17 +7,24 @@ import api from '../service/api';
  */
 
 export async function fetchZip(zip) {
-  var state;
-  var city;
-  var streetAddress;
-  var neighborhood;
+  var state = '';
+  var city = '';
+  var streetAddress = '';
+  var neighborhood = '';
 
-  const { data } = await api.get(`https://viacep.com.br/ws/${zip}/json/`);
+  try {
+    const { data } = await api.get(`https://viacep.com.br/ws/${zip}/json/`);
 
-  state = data.uf;
-  city = data.localidade;
-  streetAddress = data.logradouro;
-  neighborhood = data.bairro;
+    state = data.uf;
+    city = data.localidade;
+    streetAddress = data.logradouro;
+    neighborhood = data.bairro;
+  } catch {
+    state = undefined;
+    city = undefined;
+    streetAddress = undefined;
+    neighborhood = undefined;
+  }
 
   return { state, city, streetAddress, neighborhood };
 }
