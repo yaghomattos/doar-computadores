bdac4aa4f3e183c25181e53bc7af952500b892b4
Yagho Mattos <mattosyagho@gmail.com>
2022-08-04 00:51:37 -0300

    feature: institutions page base

--- src/pages/instituicoes.js
+++ src/pages/instituicoes.js
@@ -1,5 +1,62 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
+import data from '../service/institutions.json';
 
-export default function Institution() {
-  return <h1 className="text-black">Hello Instituições!</h1>;
+export default function Institutions() {
+  const orgs = data.orgs;
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center bg-gray-200">
+      <Head>
+        <title>Instituições</title>
+      </Head>
+      <div className="shadow-xl mx-auto max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-2xl px-8 my-5 bg-white rounded-md">
+        <header className="my-5">
+          <h1 className="text-zinc-600">
+            Instituições que podem receber doações:
+          </h1>
+        </header>
+
+        {orgs.map((org, index) => {
+          return (
+            <div key={index} className="border border-1 mb-2">
+              <div className="text-zinc-400">
+                <h1>{orgs[index].name}</h1>
+                <div className="w-full flex flex-wrap">
+                  <h2>Cidade:</h2>
+                  <p>{orgs[index].city}</p>
+                </div>
+                <div className="w-full flex flex-wrap">
+                  <h2>Bairro:</h2>
+                  <p>{orgs[index].neighboor}</p>
+                </div>
+                <div className="flex flex-wrap text-justify">
+                  <h2>Descrição:</h2>
+                  <p>{orgs[index].presentation}</p>
+                </div>
+              </div>
+
+              <footer className="mt-4 text-zinc-400">
+                <Link href={''}>
+                  <a>{orgs[index].links.site}</a>
+                </Link>
+                <Link href={''}>
+                  <a className="flex flex-wrap">
+                    {orgs[index].links.instagram}
+                  </a>
+                </Link>
+                <Link href={''}>
+                  <a>{orgs[index].links.facebook}</a>
+                </Link>
+                <Link href={''}>
+                  <a>{orgs[index].links.whatsapp}</a>
+                </Link>
+              </footer>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
