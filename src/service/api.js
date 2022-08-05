c503e701d2b45a74cfac837893093c82c7a697ba
Yagho Mattos <mattosyagho@gmail.com>
2022-08-04 23:34:00 -0300

    fix: blocked by CORS policy

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://aleks-doar-compautadores.herokuapp.com/',
-  headers: { 'content-type': 'application/json; charset=utf-8' },
+  baseURL:
+    'https://corsanywhere.herokuapp.com/https://aleks-doar-compautadores.herokuapp.com',
+  headers: {
+    'content-type': 'application/json; charset=utf-8',
+  },
 });
 
 export default api;
