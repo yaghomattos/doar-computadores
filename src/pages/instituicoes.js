72b7e10ad41e2a9285f94bda6bf47eb093cb3b30
Yagho Mattos <mattosyagho@gmail.com>
2022-08-06 23:13:08 -0300

    style: institutions responsiveness

--- src/pages/instituicoes.js
+++ src/pages/instituicoes.js
@@ -13,16 +13,16 @@ export default function Institutions() {
         <title>Instituições</title>
       </Head>
 
-      <div className="w-8/12 mx-auto my-5 px-5 bg-white rounded-md">
+      <div className="w-min sm:w-7/12 mx-auto my-5 px-5 bg-white rounded-md">
         <header className="mb-5 mt-5 pb-6">
           <Link href={'/'}>
             <div className="w-fit flex mb-8 mt-4 py-2 px-4 gap-2 items-center bg-green-500 rounded-md cursor-pointer">
-              <BiArrowBack size="24" />
-              <span className="font-bold text-lg">Fazer doação</span>
+              <BiArrowBack size="24" color="white" />
+              <a className="font-bold text-lg text-white">Fazer doação</a>
             </div>
           </Link>
 
-          <h1 className="text-center text-5xl font-extrabold text-zinc-700">
+          <h1 className="text-center text-4xl sm:text-5xl font-extrabold text-zinc-700">
             Instituições que podem receber doações
           </h1>
         </header>
@@ -31,43 +31,37 @@ export default function Institutions() {
           return (
             <div key={index} className="mb-5 pt-8 border-t-2">
               <div className="text-zinc-400">
-                <h2 className="text-center text-xl font-bold text-zinc-600">
-                  {orgs[index].name}
+                <h2 className="mb-5 text-center text-xl font-bold text-zinc-600">
+                  {org.name}
                 </h2>
-                <div className="w-full flex flex-wrap gap-1 items-center">
-                  <h3 className="text-lg font-bold text-zinc-500">Cidade:</h3>
-                  <p className="text-lg font-normal text-zinc-400">
-                    {orgs[index].city}
-                  </p>
+                <div className="w-full flex flex-wrap gap-1 items-center text-lg">
+                  <h3 className="font-bold text-zinc-500">Cidade:</h3>
+                  <p className="font-normal text-zinc-400">{org.city}</p>
                 </div>
-                <div className="w-full flex flex-wrap gap-1 items-center">
-                  <h3 className="text-lg font-bold text-zinc-500">Bairro:</h3>
-                  <p className="text-lg font-normal text-zinc-400">
-                    {orgs[index].neighboor}
-                  </p>
+                <div className="w-full flex flex-wrap gap-1 items-center text-lg">
+                  <h3 className="font-bold text-zinc-500">Bairro:</h3>
+                  <p className="font-normal text-zinc-400">{org.neighboor}</p>
                 </div>
-                <div className="flex flex-wrap text-justify">
-                  <h3 className="text-lg font-bold text-zinc-500">
-                    Descrição:
-                  </h3>
-                  <p className="text-lg font-normal text-zinc-400">
-                    {orgs[index].presentation}
+                <div className="flex flex-wrap text-justify text-lg">
+                  <h3 className="font-bold text-zinc-500">Descrição:</h3>
+                  <p className="font-normal text-zinc-400">
+                    {org.presentation}
                   </p>
                 </div>
               </div>
 
-              <footer className="w-full flex flex-row gap-10 mt-4 text-zinc-400">
-                <Link href={`${orgs[index].links.site}`}>
-                  <a className="text-lg font-bold text-zinc-400">Site</a>
+              <footer className="grid grid-cols-2 md:grid-cols-4 mt-4 text-center text-lg font-bold text-zinc-400">
+                <Link href={'https://www.appmasters.io/'}>
+                  <a target="_blank">Site</a>
                 </Link>
-                <Link href={`${orgs[index].links.instagram}`}>
-                  <a className="text-lg font-bold text-zinc-400">Instagram</a>
+                <Link href={'https://www.appmasters.io/'}>
+                  <a target="_blank">Instagram</a>
                 </Link>
-                <Link href={`${orgs[index].links.facebook}`}>
-                  <a className="text-lg font-bold text-zinc-400">Facebook</a>
+                <Link href={'https://www.appmasters.io/'}>
+                  <a target="_blank">Facebook</a>
                 </Link>
-                <Link href={`${orgs[index].links.whatsapp}`}>
-                  <a className="text-lg font-bold text-zinc-400">Whatsapp</a>
+                <Link href={'https://www.appmasters.io/'}>
+                  <a target="_blank">Whatsapp</a>
                 </Link>
               </footer>
             </div>
